0c94d042da7101c449fdb4f418332ef62d9475bd
Harry Owen <harry@twistedvin.es>
2017-11-05 14:11:16 +0000

    Wrap usermod in conditional
    ... the group won't always exist

--- tidy-up.sh
+++ tidy-up.sh
@@ -2,8 +2,11 @@
 
 # This script tidies up after the installation process.
 
-# revoke hobag's nopasswd group membership
-gpasswd -d hobag nopasswd
+passwd_group="$(grep "nopasswd" /etc/group)"
+if [ -n "$passwd_group" ]; then
+  # revoke hobag's nopasswd group membership
+  gpasswd -d hobag nopasswd
+fi
 
 # add hobag to wheel group for password sudo
 usermod -a -G wheel hobag
